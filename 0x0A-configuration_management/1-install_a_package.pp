d2d5f167557404db257184b01e93d95f121e83da
scaarifintl <scaarifintl@gmail.com>
2023-01-11 10:11:24 +0300

    use package to install, instead of an actual command (with exec)

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Install Flask from pip3
 # Flask version should be 2.1.0
 
-exec {'pip3 install Flask':
-  command => '/usr/bin/pip install -Iv Flask==1.2.2'
+package {'Flask':
+  ensure   => 'latest',
+  provider => 'pip3',
 }
