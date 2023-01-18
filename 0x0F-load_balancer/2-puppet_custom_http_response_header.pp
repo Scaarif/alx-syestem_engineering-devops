3104742d223a7f75f3aa2046368ee253c8a8c7c7
scaarifintl <scaarifintl@gmail.com>
2023-01-18 16:25:11 +0700

    test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,16 +2,14 @@
 
 # update packages
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+  command => 'sudo apt-get -y update',
+  before  => Exec['install nginx'],
 }
 
 # install nginx
 exec {'install nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
+  command => 'sudo apt-get -y install nginx',
+  before  => Exec['add_header'],
 }
 
 # add custom header in configuration
@@ -19,11 +17,10 @@ exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+  before      => Exec['restart nginx'],
 }
 
 # restart nginx once done with configuration
-exec { 'restart Nginx':
-  provider => shell,
+exec { 'restart nginx':
   command  => 'sudo service nginx restart',
 }
