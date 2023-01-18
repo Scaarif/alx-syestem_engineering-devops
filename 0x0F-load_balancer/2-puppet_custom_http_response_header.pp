365c36377d9f1704b367aa199704c0a244c71713
scaarifintl <scaarifintl@gmail.com>
2023-01-18 16:05:35 +0700

    ....

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,25 @@
-# Automate the task of creating a :wq
-# custom HTTP header response
+# Installs a Nginx server with custome HTTP header
 
-# update packages
-exec { 'update':
-  command => 'sudo apt-get -y update',
-  before  => Exec['install Nginx'],
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-# install nginx
-exec { 'install nginx':
-  command => 'sudo apt-get -y install nginx',
-  before  => Exec['add_header'],
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-# add the custom header
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i \'49i\\tadd_header X-Served-By "$HOST";\' /etc/nginx/sites-available/default',
-  before      => Exec ['restart Nginx'],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-# restart nginx
-exec {'restart nginx':
-  command => 'sudo service nginx restart',
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
