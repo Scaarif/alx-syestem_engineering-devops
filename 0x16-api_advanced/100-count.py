b8706fe4ad6dca5926a3822f85846354bf2668ad
scaarifintl <scaarifintl@gmail.com>
2023-03-01 21:36:41 +0300

    handle duplicate keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -19,7 +19,11 @@ def count_words(subreddit, word_list):
         setattr(count_words, 'params', {'limit': 100})
     if getattr(count_words, 'words', None) is None:
         setattr(count_words, 'words', {})
-    # print(count_words.__name__, count_words.__dict__)
+    if getattr(count_words, 'words') == {}:
+        # print('setting words attribute')
+        for word in word_list:
+            if word.lower() not in count_words.words.keys():
+                count_words.words[word.lower()] = 0
     # base case (count_words only if there are more 'things') else, return
     if (getattr(count_words, 'more') >= 100):
         res = requests.get(url, headers=headers, params=getattr(
@@ -29,34 +33,27 @@ def count_words(subreddit, word_list):
             after = res.json()['data']['after']
             data = res.json()['data']['children']
             count_words.more = len(data)
-            # print('more: ', count_words.more)
-            # print('data: ', data)
             for post in data:
                 # print(post['data']['title'])
                 for word in word_list:
-                    if not count_words.words.keys():
-                        # print('setting words attribute')
-                        for word in word_list:
-                            if not (word in count_words.words.keys()):
-                                # print(count_words.words.keys())
-                                count_words.words[word] = 0
-                    count_words.words[word] += (post['data']['title'].
-                                                lower().count(word.lower()))
-                # hot_list.append(post['data']['title'])
-            # print(count_words.words)
+                    count_words.words[word.lower()] += (post['data']['title'].
+                                                        lower().count(word.lower()))
             count_words.params['after'] = after
-            # print(count_words.params['after'])
             return count_words(subreddit, word_list)
         except Exception as e:
             # print('error: ', e)
             pass
     # print results, in descending order, by count and if the count is same,
     # alphabetically sorted ( ascending A-Z)
-    count = sorted(count_words.words.items(), key=lambda item: item[1])
-    count = dict(count)
-    for key in reversed(count):
-        if count[key] != 0:
-            print('{}: {}'.format(key.lower(), count[key]))
+    if len(word_list) > 1:
+        count = sorted(count_words.words.items(), key=lambda item: item[1])
+        count = dict(count)
+        for key in reversed(count):
+            if count[key] != 0:
+                print('{}: {}'.format(key.lower(), count[key]))
+    else:
+        print('{}: {}'.format(word_list[0].lower(),
+                              count_words.words[word_list[0].lower()]))
 
 
 if __name__ == '__main__':
